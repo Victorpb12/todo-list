d818026d7fbde7c86f56f7dfa0d950b2809a53d5
Victor Pavan Baumgartner <victorpbaumg@gmail.com>
2023-12-20 21:26:17 -0300

    first commit

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
+
+  return (
+    <div className='app'>
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <div className='todo'>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
